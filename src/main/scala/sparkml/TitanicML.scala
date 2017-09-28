e800c1bf54098d83443d6948fc8c1184c8cb8d8c
Sili Wen <siliwen@amazon.com>
2017-09-28 15:15:42 -0700

    Titanic prediction

--- src/main/scala/sparkml/TitanicML.scala
+++ src/main/scala/sparkml/TitanicML.scala
@@ -1,49 +1,57 @@
 package sparkml
 
 import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer, VectorAssembler}
+import org.apache.spark.sql.DataFrame
 
 object TitanicML {
 
   val spark = Spark.getSpark
   val trainDataFilePath = "data/titanic/train.csv"
+  val testDataFilePath = "data/titanic/test.csv"
 
-  def doML: Unit = {
+  def doML(): Unit = {
+    val training = getData(trainDataFilePath)
+    val lrModel = new LogisticRegression().fit(training)
+
+    val test = getData(testDataFilePath)
+    val predictions = lrModel.transform(test)
+
+    predictions.select("prediction").describe()
+  }
+
+  private def getData(filePath: String) : DataFrame = {
     val df = spark.read
         .option("header", true)
         .option("inferSchema", true)
         .csv(trainDataFilePath)
+        .na.fill(-1, Array("Age"))
+        .na.fill("SHIP", Array("Cabin"))
+        .na.fill("Z", Array("Embarked"))
+        .withColumnRenamed("Survived", "label")
 
-    df.printSchema()
-    for (colName <- df.columns) {
-      val col = df(colName)
-      val cnt = df.filter(col.isNaN || col.isNull || col === "").count()
+    val sexIndexer = new StringIndexer()
+      .setInputCol("Sex")
+      .setOutputCol("SexIndex")
+    val sexEncoder = new OneHotEncoder()
+      .setInputCol("SexIndex")
+      .setOutputCol("sexVector")
 
-      println(colName, cnt)
-    }
+    val cabinIndexer = new StringIndexer()
+      .setInputCol("Cabin")
+      .setOutputCol("CabinIndex")
 
-//    val sexIndexer = new StringIndexer()
-//      .setInputCol("Sex")
-//      .setOutputCol("SexIndex")
-//    val sexEncoder = new OneHotEncoder()
-//      .setInputCol("SexIndex")
-//      .setOutputCol("sexVector")
-//
-//    val cabinIndexer = new StringIndexer()
-//      .setInputCol("Cabin")
-//      .setOutputCol("CabinIndex")
-//
-//    val embarkedIndexer = new StringIndexer()
-//      .setInputCol("Embarked")
-//      .setOutputCol("EmbarkedIndex")
-//
-//    val assembler = new VectorAssembler()
-//      .setInputCols(Array("Pclass", "sexVector", "Age", "SibSp", "Parch", "Fare", "CabinIndex", "EmbarkedIndex"))
-//        .setOutputCol("features")
-//
-//    val pipeline = new Pipeline().setStages(Array(sexIndexer, sexEncoder, cabinIndexer, embarkedIndexer, assembler))
-//    val output = pipeline.fit(df).transform(df)
-//    output.show()
+    val embarkedIndexer = new StringIndexer()
+      .setInputCol("Embarked")
+      .setOutputCol("EmbarkedIndex")
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("Pclass", "sexVector", "Age", "SibSp", "Parch", "Fare", "CabinIndex", "EmbarkedIndex"))
+        .setOutputCol("features")
+
+    val pipeline = new Pipeline().setStages(Array(sexIndexer, sexEncoder, cabinIndexer, embarkedIndexer, assembler))
+    pipeline.fit(df).transform(df).select("features", "label")
   }
 
   def main(args: Array[String]): Unit = {
