09a908b1a67e425616de5cd5f79c108a5777a738
Sili Wen <siliwen@amazon.com>
2017-10-04 13:57:11 -0700

    set ops

--- src/main/scala/scalaexercises/stdlib/sets/SetUses.scala
+++ src/main/scala/scalaexercises/stdlib/sets/SetUses.scala
@@ -2,4 +2,26 @@ package scalaexercises.stdlib.sets
 
 object SetUses {
 
+  def setAdditionRemove(): Unit = {
+    val states = Set("MI", "OH", "WI", "MN")
+    println(states.size)
+
+    val moreStates = states + "WA"
+
+    val newStates = states -- Set("MI", "OH")
+    val newStates1 = states - ("MI", "OH")
+
+    val newStates2 = states - "NY"
+    print(states.equals(newStates2))
+  }
+
+  def setOperations(): Unit = {
+
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    setOperations()
+  }
+
 }
